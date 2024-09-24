bf07c290bd8d3f0950ab045d32d272df8d707ed5
neclor <neclordev@gmail.com>
2024-09-24 09:02:18 +0200

    b

--- BucketAI/Game.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-struct Point {
-    unsigned x;
-    unsigned y;
-
-    Point(unsigned x = 0, unsigned y = 0) : x(x), y(y) {}
-};
--- Snake/Snake.c
+++ Snake/Snake.c
@@ -1,79 +1,161 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <conio.h>
+#include <windows.h>
 
-
-#define FIELD_POSITION_X 1
-#define FIELD_POSITION_Y 1
+#define UP 0
+#define RIGHT 1
+#define DOWN 2
+#define LEFT 3
 
 #define FIELD_WIDTH 20
 #define FIELD_HEIGHT 20
 
-#define WALL_CHAR "#"
-#define FLOOR_CHAR "."
-#define SNAKE_HEAD_CHAR "@"
-#define SNAKE_CHAR "0"
-#define APPLE_CHAR "$"
+#define EMPTY_CELL 0
+#define SNAKE_CELL 1
+#define APPLE_CELL 2
+
+#define FLOOR_CHAR '.'
+#define SNAKE_CHAR '@'
+#define APPLE_CHAR '$'
 
 typedef struct {
-    int x;
-    int y;
+    int x, y;
 } Point;
 
-int gameOver;
-int score;
+int field[FIELD_WIDTH][FIELD_HEIGHT] = { EMPTY_CELL };
 
-int fieldArea = FIELD_WIDTH * FIELD_HEIGHT;
-int field[FIELD_WIDTH][FIELD_HEIGHT] = { 0 };
+int gameOver;
 
 Point snakeBody[FIELD_WIDTH * FIELD_HEIGHT];
 int snakeLenght;
-int direction; // 0 - Up, 1 - Right, 2 - Down, 3 - Left
+int direction;
 
 void drawField() {
-    /*
-    for (int x = FIELD_POSITION_X - 1; x <= FIELD_POSITION_X + FIELD_WIDTH; x++) {
-        printf("\033[%d;%dH", x, FIELD_POSITION_Y - 1);
-        printf("\033[%d;%dH", x, FIELD_POSITION_Y + FIELD_HEIGHT);
-    }
-
-    for (int y = FIELD_POSITION_Y - 1; y <= FIELD_POSITION_Y + FIELD_HEIGHT; y++) {
-        printf("\033[%d;%dH", FIELD_POSITION_X - 1, y);
-        printf("\033[%d;%dH", FIELD_POSITION_X + FIELD_WIDTH, y);
-    }
-    */
-    
-    for (int x = FIELD_POSITION_X - 1; x <= FIELD_POSITION_X + FIELD_WIDTH; x++) {
-        for (int y = FIELD_POSITION_Y - 1; y <= FIELD_POSITION_Y + FIELD_HEIGHT; y++) {
-            printf("\033[%d;%dH", x, y);
-            if (x == FIELD_POSITION_X - 1 || x == FIELD_POSITION_X + FIELD_WIDTH || y == FIELD_POSITION_Y - 1 || y == FIELD_POSITION_Y + FIELD_HEIGHT) {
-                printf("WALL_CHAR");
-            }
-            else {
-                printf("FLOOR_CHAR");
+    char screenData[FIELD_HEIGHT][FIELD_WIDTH + 1];
+    for (int y = 0; y < FIELD_HEIGHT; y++) {
+        screenData[y][FIELD_WIDTH] = '\n';
+        for (int x = 0; x < FIELD_WIDTH; x++) {
+            switch (field[x][y]) {
+                case(EMPTY_CELL):
+                    screenData[y][x] = FLOOR_CHAR;
+                    break;
+                case(SNAKE_CELL):
+                    screenData[y][x] = SNAKE_CHAR;
+                    break;
+                case(APPLE_CELL):
+                    screenData[y][x] = APPLE_CHAR;
+                    break;
             }
         }
-    } 
+    }
+    screenData[FIELD_HEIGHT - 1][FIELD_WIDTH] = 0;
+    printf("\033[%d;%dH", 1, 1);
+    puts(screenData);
 }
 
 void drawScore() {
-    printf("\033[%d;%dH", FIELD_POSITION_X, FIELD_POSITION_Y + FIELD_HEIGHT + 2);
-    printf("Score: %d", score);
+    printf("\033[%d;%dH", 1, FIELD_WIDTH + 2);
+    printf("Score: %d", snakeLenght - 1);
 }
 
-void drawGameOver() {
-
+void getDirection() {
+    int newDirection;
+    int key = _getch();
+    switch (key) {
+        case 119: // w
+        case 87: // W
+            newDirection = UP;
+            break;
+        case 100: // d
+        case 68: // D
+            newDirection = RIGHT;
+            break;
+        case 115: // s
+        case 83: // S
+            newDirection = DOWN;
+            break;
+        case 97: // a
+        case 65: // A
+            newDirection = LEFT;
+            break;
+        default:
+            newDirection = direction;
+    }
+    direction = (direction % 2 == newDirection % 2) ? direction : newDirection;
 }
 
+void createApple() {
+    int randomPosition = rand() % (FIELD_WIDTH * FIELD_HEIGHT - snakeLenght);
+    int count = 0;
+    for (int x = 0; x < FIELD_WIDTH; x++) {
+        for (int y = 0; y < FIELD_HEIGHT; y++) {
+            if (field[x][y] == EMPTY_CELL) {
+                if (randomPosition == count) {
+                    field[x][y] = APPLE_CELL;
+                    return;
+                }
+                count++;
+            }
+        }
+    }
+}
 
-void init() {
+void moveSnake() {
+    Point nextPosition = snakeBody[snakeLenght - 1];
+
+    switch (direction) {
+        case UP:
+            nextPosition.y--;
+            break;
+        case RIGHT:
+            nextPosition.x++;
+            break;
+        case DOWN:
+            nextPosition.y++;
+            break;
+        case LEFT:
+            nextPosition.x--;
+            break;
+    }
+    nextPosition.x = (nextPosition.x + FIELD_WIDTH) % FIELD_WIDTH;
+    nextPosition.y = (nextPosition.y + FIELD_HEIGHT) % FIELD_HEIGHT;
+
+    if (field[nextPosition.x][nextPosition.y] == SNAKE_CELL) {
+        gameOver = 1;
+        return;
+    }
+    else if (field[nextPosition.x][nextPosition.y] == APPLE_CELL) {
+        snakeLenght++;
+        drawScore();
+        if (snakeLenght == FIELD_WIDTH * FIELD_WIDTH) {
+            gameOver = 1;
+            return;
+        }
+        createApple();
+    }
+    else {
+        field[snakeBody[0].x][snakeBody[0].y] = EMPTY_CELL;
+        for (int i = 0; i < snakeLenght - 1; i++) {
+            snakeBody[i] = snakeBody[i + 1];
+        }
+    }
+    snakeBody[snakeLenght - 1] = nextPosition;
+    field[nextPosition.x][nextPosition.y] = SNAKE_CELL;
+}
+
+void restart() {
+    for (int x = 0; x < FIELD_WIDTH; x++) {
+        for (int y = 0; y < FIELD_HEIGHT; y++) {
+            field[x][y] = EMPTY_CELL;
+        }
+    }
     Point snakeStartPosition = { FIELD_WIDTH / 2, FIELD_HEIGHT / 2 };
     snakeBody[0] = snakeStartPosition;
     snakeLenght = 1;
-    direction = 0;
+    direction = UP;
 
-    field[snakeStartPosition.x][snakeStartPosition.y] = 1;
+    field[snakeStartPosition.x][snakeStartPosition.y] = SNAKE_CELL;
     createApple();
 
     gameOver = 0;
@@ -82,98 +164,20 @@ void init() {
     drawScore();
 }
 
-void createApple() {
-    int value = rand() % (fieldArea - snakeLenght + 1);
-    int count = 0;
-    for (int x = 0; x < FIELD_WIDTH; x++) {
-        for (int y = 0; y < FIELD_HEIGHT; y++) {
-            if (value == count) {
-                field[x][y] = 2;
-                printf("\033[%d;%dH", FIELD_POSITION_X + x, FIELD_POSITION_Y + y);
-                printf(APPLE_CHAR);
-                return;
-            }
-            count++;
-        }
-    }
-}
-
-void getDirection() {
-    int newDirection;
-    int key = _getch();
-    switch(key) {
-        case 119: // w
-        case 87: // W
-            newDirection = 0;
-        case 100: // d
-        case 68: // D
-            newDirection = 1;
-        case 115: // s
-        case 83: // S
-            newDirection = 2;
-        case 97: // a
-        case 65: // A
-            newDirection = 3;
-        default:
-            newDirection = direction;
-    }
-    direction = (direction % 2 == newDirection % 2) ? direction : newDirection;
-}
-
-void moveSnake() {
-    printf("%s", "\033[1;32m");
-    printf("This text is green!\n");
-
-
-    Point nextPosition = snakeBody[0];
-    switch(direction) {
-        case 0:
-            nextPosition.y--;
-        case 1:
-            nextPosition.x++;
-        case 2:
-            nextPosition.y++;
-        case 3:
-            nextPosition.x--;
-    }
-    nextPosition.x = (nextPosition.x + FIELD_WIDTH) % FIELD_WIDTH;
-    nextPosition.y = (nextPosition.y + FIELD_HEIGHT) % FIELD_HEIGHT;
-
-    if(field[nextPosition.x][nextPosition.y] == 1) {
-        gameOver = 1;
-        return;
-    }
-
-    if(field[nextPosition.x][nextPosition.y] == 2) {
-        score++;
-        snakeLenght++;
-        if(snakeLenght == fieldArea) {
-            gameOver = 1;
-        }
-    }
-    
-
-
-    memmove(snakeBody + 1, snakeBody, snakeLenght * sizeof(int));
-
-    snakeBody;
-
-
-
-
-
-
-
-
+void init() {
+    printf("\033[?25l");
 }
 
 int main() {
-    while(1) {
-        init();
-        while(!gameOver) {
+    int a = 0;
+    init();
+    while (1) {
+        restart();
+        while (!gameOver) {
             getDirection();
-
-
+            moveSnake();
+            drawField();
+            Sleep(200);
         }
     }
     return 0;
